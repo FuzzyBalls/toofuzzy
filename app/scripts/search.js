7bec9b0644fc6361b88fefdffc8296ed313567cc
Kendall Morgan <kendall@Kendalls-MacBook-Pro.local>
2014-09-28 11:00:06 -0700

    refactor

--- app/scripts/search.js
+++ app/scripts/search.js
@@ -1,23 +1,20 @@
 	$('body').prepend($('<input id="fuzzysearchbox">'));
 
-	var parse = function(){
+	var parse = function()
 		return document.getElementsByTagName('p');
 	}
 
 	var doSearch = function(needle, haystack){
-		var f = new Fuse(parse(), { keys: ['innerText']});
+		var f = new Fuse(haystack, { keys: ['innerText']});
 		var results = f.search(needle);
 		console.log(results[0].innerText);
 	}
 
 	$(document).ready(function(){
 
-		var pTags = parse();
-		console.log(pTags);
-
 		$('#fuzzysearchbox').on('input', function(e){
 			var searchTerm = e.currentTarget.value;
-			doSearch(searchTerm, pTags);
+			doSearch(searchTerm, parse());
 		});
 	});
 
