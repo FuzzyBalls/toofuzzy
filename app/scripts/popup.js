3d3077d39e6bd1d471524c5333711dd7cde7a6ca
Ngoc Do <ngocmdo@gmail.com>
2014-09-28 16:24:58 -0700

    add clear on popup close

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,9 +1,17 @@
 'use strict';
 
+var port;
 $(document).ready(function(){
 	$("#searchBox").on("input", function(){
 		var searchQuery = $("#searchBox").val();
 		sendQuery(searchQuery);
+	});
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		// chrome.tabs.sendMessage(tabs[0].id, {searchQuery: searchQuery}, function(response) {
+		// 	console.log(response.farewell);
+		// });
+
+		port = chrome.tabs.connect(tabs[0].id, {name: "fuzzypopup"})
 	});
 });
 
@@ -12,9 +20,10 @@ $(window).unload(function(){
 });
 
 var sendQuery = function(searchQuery){
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, {searchQuery: searchQuery}, function(response) {
-			console.log(response.farewell);
-		});
-	});
+	// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	// 	chrome.tabs.sendMessage(tabs[0].id, {searchQuery: searchQuery}, function(response) {
+	// 		console.log(response.farewell);
+	// 	});
+	// });
+	port.postMessage({searchQuery: searchQuery});
 };
--- app/scripts/search.js
+++ app/scripts/search.js
@@ -1,29 +1,31 @@
 var fuzzySearch = (function($) {
 
     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        // console.log(sender.tab ?
+        // "from a content script:" + sender.tab.url :
+        // "from the extension");
+        // if (request.searchQuery) {
+        // console.log("got search query: '" + request.searchQuery + "'");
         startTheFireworks(request.searchQuery);
         sendResponse({
             farewell: "goodbye"
         });
+        // }
     });
 
     chrome.runtime.onConnect.addListener(function(port) {
-        console.assert(port.name == "knockknock");
+        console.assert(port.name == "fuzzypopup");
+
         port.onMessage.addListener(function(msg) {
-            if (msg.joke == "Knock knock")
-                port.postMessage({
-                    question: "Who's there?"
-                });
-            else if (msg.answer == "Madame")
-                port.postMessage({
-                    question: "Madame who?"
-                });
-            else if (msg.answer == "Madame... Bovary")
-                port.postMessage({
-                    question: "I don't get it."
-                });
+        	console.log(msg.searchQuery);
+        	startTheFireworks(msg.searchQuery);
+        });
+
+        port.onDisconnect.addListener(function(){
+        	startTheFireworks("");
         });
     });
+
 
     var parse = function() {
         var parseResults = [];
