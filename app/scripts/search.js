d9c27612afb4db7e3c576f64634e7f0be13b3164
Ngoc Do <ngocmdo@gmail.com>
2014-09-27 21:05:45 -0700

    fix search

--- app/scripts/search.js
+++ app/scripts/search.js
@@ -1,35 +1,46 @@
 'use strict';
 
-$(function(){
+var FuzzySearch = (function($){
 
-	$('#searchBox').on('input', function(e){
-		var searchTerm = e.currentTarget.value;
-		doSearch(searchTerm, "");
+	$(document).ready(function(){
+		$('#searchBox').on('input', function(e){
+			var searchTerm = e.currentTarget.value;
+			doSearch(searchTerm, "");
+		});
 	});
 
-	// Returns an array of all dom elements on the page containing text 
-	function parse() {
-	  var $domElements = $("body");
-	  var results = []; 
-	  // edge case for stuff around <head></head> tag
-	  findText($domElements[0], results);
-	  return results;
+	var parse = function(){
+		return $("p");
 	}
 
-	function findText($domElement, results) {
-	  if(domElement) {
-	    if($domElement.innerText) {
-	      results.push($domElement);
-	    } 
-	    var $domChildren = $domElement.children;
-	    if($domChildren) {
-	      for(c : $domChildren) {
-	        findText(c, results);
-	      }
-	    }
-	  }
-	  return results;
+	// Returns an array of all dom elements on the page containing text 
+	// function parse() {
+	//   var $domElements = $("body");
+	//   var results = []; 
+	//   // edge case for stuff around <head></head> tag
+	//   findText($domElements[0], results);
+	//   return results;
+	// }
+
+	// function findText($domElement, results) {
+	//   if($domElement) {
+	//     if($domElement.innerText) {
+	//       results.push($domElement);
+	//     } 
+	//     var $domChildren = $domElement.children;
+	//     if($domChildren) {
+	//       for(var c in $domChildren) {
+	//         findText(c, results);
+	//       }
+	//     }
+	//   }
+	//   return results;
+	// }
+
+	return {
+		parse: parse,
+		findText: findText
 	}
-});
+})(jQuery);
 
 
