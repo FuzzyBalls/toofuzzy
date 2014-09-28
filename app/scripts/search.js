268f2d64aaed727dc713c00a9ce490065839c0ad
Kendall Morgan <kendall@Kendalls-MacBook-Pro.local>
2014-09-28 09:57:40 -0700

    Getting fuzzy on they asses

--- app/scripts/search.js
+++ app/scripts/search.js
@@ -1,46 +1,76 @@
-'use strict';
+	$('body').prepend($('<input id="fuzzysearchbox">'));
+	var parse = function(){
+		return document.getElementsByTagName('p');
+	}
 
-var FuzzySearch = (function($){
+	var doSearch = function(needle, haystack){
+		var f = new Fuse(parse(), { keys: ['innerText']});
+		var results = f.search(needle);
+		console.log(results[0].innerText);
+	}
 
 	$(document).ready(function(){
-		$('#searchBox').on('input', function(e){
+
+		var pTags = parse();
+		console.log(pTags);
+
+		$('#fuzzysearchbox').on('input', function(e){
 			var searchTerm = e.currentTarget.value;
-			doSearch(searchTerm, "");
+			doSearch(searchTerm, pTags);
 		});
 	});
+// 'use strict';
 
-	var parse = function(){
-		return $("p");
-	}
+// var FuzzySearch = (function($){
 
-	// Returns an array of all dom elements on the page containing text 
-	// function parse() {
-	//   var $domElements = $("body");
-	//   var results = []; 
-	//   // edge case for stuff around <head></head> tag
-	//   findText($domElements[0], results);
-	//   return results;
-	// }
+// 	var parse = function(){
+// 		return document.getElementsByTagName('p');
+// 	}
 
-	// function findText($domElement, results) {
-	//   if($domElement) {
-	//     if($domElement.innerText) {
-	//       results.push($domElement);
-	//     } 
-	//     var $domChildren = $domElement.children;
-	//     if($domChildren) {
-	//       for(var c in $domChildren) {
-	//         findText(c, results);
-	//       }
-	//     }
-	//   }
-	//   return results;
-	// }
+// 	var doSearch = function(needle, haystack){
+// 		var f = new Fuse(parse(), { keys: []});
+// 	}
 
-	return {
-		parse: parse,
-		findText: findText
-	}
-})(jQuery);
+// 	$(document).ready(function(){
+
+// 		var pTags = parse();
+// 		console.log(pTags);
+
+// 		$('#searchBox').on('input', function(e){
+// 			var searchTerm = e.currentTarget.value;
+// 			doSearch(searchTerm, "");
+// 		});
+// 	});
+
+
+// 	// Returns an array of all dom elements on the page containing text 
+// 	// function parse() {
+// 	//   var $domElements = $("body");
+// 	//   var results = []; 
+// 	//   // edge case for stuff around <head></head> tag
+// 	//   findText($domElements[0], results);
+// 	//   return results;
+// 	// }
+
+// 	// function findText($domElement, results) {
+// 	//   if($domElement) {
+// 	//     if($domElement.innerText) {
+// 	//       results.push($domElement);
+// 	//     } 
+// 	//     var $domChildren = $domElement.children;
+// 	//     if($domChildren) {
+// 	//       for(var c in $domChildren) {
+// 	//         findText(c, results);
+// 	//       }
+// 	//     }
+// 	//   }
+// 	//   return results;
+// 	// }
+
+// 	return {
+// 		parse: parse
+// 		// findText: findText
+// 	}
+// })(jQuery);
 
 
